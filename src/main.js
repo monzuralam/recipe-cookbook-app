1d816c9af4a99107bb79f3a9b0070db44aa320d5
monzuralam <monzur707@gmail.com>
2023-09-29 21:45:25 +0600

    router config

--- src/main.js
+++ src/main.js
@@ -1,10 +1,32 @@
-import { createApp } from 'vue'
-import { createPinia } from "pinia"
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import { createPinia } from "pinia";
+
+import Home from "./routes/Home.vue";
+import Errors from "./routes/Errors.vue";
+
+import "./style.css";
+import App from "./App.vue";
+
+const routes = [
+  {
+    path: "/",
+    component: Home,
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    component: Errors,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
 
 const app = createApp(App);
 const pinia = createPinia();
 
-app.use(pinia)
-app.mount('#app')
+app.use(pinia);
+app.use(router);
+app.mount("#app");
