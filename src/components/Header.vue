7fc00ba9aeaf4a3459f301d4e65cd2debe2dbac7
monzuralam <monzur707@gmail.com>
2023-09-29 16:00:57 +0600

    Header has been created

---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<script setup>
+import "@fontsource/hind-siliguri/600.css";
+</script>
+<template>
+  <header class="my-1">
+    <h1 class="text-3xl leading-normal font-semibold text-center">
+      রেসিপি কুকবুক অ্যাপ
+    </h1>
+  </header>
+</template>
