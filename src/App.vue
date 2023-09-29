427da77f78c6dded26487c260dc4492abb68d7bf
monzuralam <monzur707@gmail.com>
2023-09-29 16:36:14 +0600

    Footer component has been added

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
-  <div class="max-w-sm mx-auto relative">
+  <div class="max-w-sm mx-auto overflow-auto relative">
     <Header />
+    <Footer />
   </div>
 </template>
 
