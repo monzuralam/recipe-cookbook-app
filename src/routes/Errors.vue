09b0a51ae9835b9a0938982cfb0b50d06cd2eba1
monzuralam <monzur707@gmail.com>
2023-09-29 21:22:55 +0600

    404 page has been added

---
+++ src/routes/Errors.vue
@@ -0,0 +1,18 @@
+<script setup>
+import "@fontsource/hind-siliguri/700.css";
+</script>
+
+<template>
+  <section class="py-40">
+    <div class="container">
+      <img
+        src="../assets/404.png"
+        alt="recipe"
+        class="grayscale w-1/2 mx-auto mb-5"
+      />
+      <h2 class="text-3xl text-center dark:text-white font-bold">
+        পাতাটি খুঁজে পাওয়া যায়নি!
+      </h2>
+    </div>
+  </section>
+</template>
