9a14f5d7edfdbb8bfade6fda7d5970647cc566a3
monzuralam <monzur707@gmail.com>
2023-09-29 21:46:11 +0600

    router-view has been added

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <script setup>
 import Header from "./components/Header.vue";
-import Home from "./routes/Home.vue";
 import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <div class="max-w-sm max-h-screen mx-auto overflow-y relative">
     <Header />
-    <Home />
+    <router-view></router-view>
     <Footer />
   </div>
 </template>
