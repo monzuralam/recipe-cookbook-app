783f1a5d5ec8f4ea93bc1479860711376cdb65ea
monzuralam <monzur707@gmail.com>
2023-09-29 15:35:14 +0600

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import "@fontsource/hind-siliguri/700.css";
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold">রেসিপি কুকবুক অ্যাপ</h1>
+  </div>
+</template>
+
+<style scoped></style>
