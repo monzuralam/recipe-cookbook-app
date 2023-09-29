a3e6bcc7a6622d7cb6b503744542bc873de080b5
monzuralam <monzur707@gmail.com>
2023-09-29 18:47:34 +0600

    Home added on home

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Header from "./components/Header.vue";
+import Home from "./routes/Home.vue";
 import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <div class="max-w-sm mx-auto overflow-auto relative">
     <Header />
+    <Home />
     <Footer />
   </div>
 </template>
