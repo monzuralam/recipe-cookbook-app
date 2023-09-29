6e45b602c306c1e78568b500e0ed48ccfd2f1b8e
monzuralam <monzur707@gmail.com>
2023-09-29 18:47:23 +0600

    Homecompoent added

---
+++ src/routes/Home.vue
@@ -0,0 +1,150 @@
+<script setup>
+import "@fontsource/hind-siliguri/500.css";
+</script>
+
+<template>
+  <div class="container">
+    <div class="grid grid-cols-2 gap-2">
+      <!-- category item start here -->
+      <div class="rounded-md border">
+        <a href="#">
+          <img
+            src="../assets/images/category/biriyani.jpg"
+            alt="Recipe"
+            class="rounded-t-md max-w-full h-28 object-cover"
+          />
+          <h3 class="text-lg font-medium leading-normal text-center py-1.5">
+            বিরিয়ানি রেসিপি
+          </h3>
+        </a>
+      </div>
+      <!-- category item stop here -->
+      <!-- category item start here -->
+      <div class="rounded-md border">
+        <a href="#">
+          <img
+            src="../assets/images/category/kabab.jpg"
+            alt="Recipe"
+            class="rounded-t-md max-w-full h-28 object-cover"
+          />
+          <h3 class="text-lg font-medium leading-normal text-center py-1.5">
+            কাবাব রেসিপি
+          </h3>
+        </a>
+      </div>
+      <!-- category item stop here -->
+      <!-- category item start here -->
+      <div class="rounded-md border">
+        <a href="#">
+          <img
+            src="https://images.unsplash.com/photo-1517115358639-5720b8e02219"
+            alt="Recipe"
+            class="rounded-t-md max-w-full h-28 object-cover"
+          />
+          <h3 class="text-lg font-medium leading-normal text-center py-1.5">
+            মাছের রেসিপি
+          </h3>
+        </a>
+      </div>
+      <!-- category item stop here -->
+      <!-- category item start here -->
+      <div class="rounded-md border">
+        <a href="#">
+          <img
+            src="https://images.unsplash.com/photo-1517115358639-5720b8e02219"
+            alt="Recipe"
+            class="rounded-t-md max-w-full h-28 object-cover"
+          />
+          <h3 class="text-lg font-medium leading-normal text-center py-1.5">
+            মাছের রেসিপি
+          </h3>
+        </a>
+      </div>
+      <!-- category item stop here -->
+      <!-- category item start here -->
+      <div class="rounded-md border">
+        <a href="#">
+          <img
+            src="https://images.unsplash.com/photo-1517115358639-5720b8e02219"
+            alt="Recipe"
+            class="rounded-t-md max-w-full h-28 object-cover"
+          />
+          <h3 class="text-lg font-medium leading-normal text-center py-1.5">
+            মাছের রেসিপি
+          </h3>
+        </a>
+      </div>
+      <!-- category item stop here -->
+      <!-- category item start here -->
+      <div class="rounded-md border">
+        <a href="#">
+          <img
+            src="https://images.unsplash.com/photo-1517115358639-5720b8e02219"
+            alt="Recipe"
+            class="rounded-t-md max-w-full h-28 object-cover"
+          />
+          <h3 class="text-lg font-medium leading-normal text-center py-1.5">
+            মাছের রেসিপি
+          </h3>
+        </a>
+      </div>
+      <!-- category item stop here -->
+      <!-- category item start here -->
+      <div class="rounded-md border">
+        <a href="#">
+          <img
+            src="https://images.unsplash.com/photo-1517115358639-5720b8e02219"
+            alt="Recipe"
+            class="rounded-t-md max-w-full h-28 object-cover"
+          />
+          <h3 class="text-lg font-medium leading-normal text-center py-1.5">
+            মাছের রেসিপি
+          </h3>
+        </a>
+      </div>
+      <!-- category item stop here -->
+      <!-- category item start here -->
+      <div class="rounded-md border">
+        <a href="#">
+          <img
+            src="https://images.unsplash.com/photo-1517115358639-5720b8e02219"
+            alt="Recipe"
+            class="rounded-t-md max-w-full h-28 object-cover"
+          />
+          <h3 class="text-lg font-medium leading-normal text-center py-1.5">
+            মাছের রেসিপি
+          </h3>
+        </a>
+      </div>
+      <!-- category item stop here -->
+      <!-- category item start here -->
+      <div class="rounded-md border">
+        <a href="#">
+          <img
+            src="https://images.unsplash.com/photo-1517115358639-5720b8e02219"
+            alt="Recipe"
+            class="rounded-t-md max-w-full h-28 object-cover"
+          />
+          <h3 class="text-lg font-medium leading-normal text-center py-1.5">
+            মাছের রেসিপি
+          </h3>
+        </a>
+      </div>
+      <!-- category item stop here -->
+      <!-- category item start here -->
+      <div class="rounded-md border">
+        <a href="#">
+          <img
+            src="https://images.unsplash.com/photo-1517115358639-5720b8e02219"
+            alt="Recipe"
+            class="rounded-t-md max-w-full h-28 object-cover"
+          />
+          <h3 class="text-lg font-medium leading-normal text-center py-1.5">
+            মাছের রেসিপি
+          </h3>
+        </a>
+      </div>
+      <!-- category item stop here -->
+    </div>
+  </div>
+</template>
