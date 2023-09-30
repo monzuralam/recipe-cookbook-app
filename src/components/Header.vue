c6bdba5ce2f3f2cf497cb1944261ba287c2fc3aa
monzuralam <monzur707@gmail.com>
2023-09-30 07:18:31 +0600

    Header Sticky to Fixed

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
 import "@fontsource/hind-siliguri/600.css";
 </script>
 <template>
-  <header class="bg-white dark:bg-black sticky top-0 py-1 mb-2">
+  <header class="bg-white dark:bg-black fixed max-w-sm w-full top-0 py-1 mb-2">
     <h1
       class="text-black dark:text-white text-3xl leading-normal font-semibold text-center"
     >
