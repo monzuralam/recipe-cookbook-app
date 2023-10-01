096d9f54b6924fd2cbb35f82cdf4bed18707fac0
monzuralam <monzur707@gmail.com>
2023-10-01 12:41:29 +0600

    Create separate component for RecipeItem & added

---
+++ src/components/Recipe/RecipeItem.vue
@@ -0,0 +1,22 @@
+<script setup></script>
+
+<template>
+  <div class="border rounded-md shadow p-1.5 mb-2.5">
+    <router-link to="/recipe/" class="flex">
+      <img
+        src="../../assets/images/category/kabab.jpg"
+        alt=""
+        class="w-12 h-12 rounded-full"
+      />
+      <div class="flex items-center mx-auto">
+        <h3
+          class="text-xl font-normal leading-normal text-center text-black dark:text-white"
+        >
+          বিফ কাবাব
+        </h3>
+      </div>
+    </router-link>
+  </div>
+</template>
+
+<style scoped></style>
--- src/routes/Recipes.vue
+++ src/routes/Recipes.vue
@@ -1,24 +1,9 @@
-<script setup></script>
+<script setup>
+import RecipeItem from "../components/Recipe/RecipeItem.vue";
+</script>
 <template>
   <div class="container h-screen py-16 dark:bg-black">
-    <!-- recipe item start here -->
-    <div class="border rounded-md shadow p-1.5 mb-2.5">
-      <router-link to="/recipe/" class="flex">
-        <img
-          src="../assets/images/category/kabab.jpg"
-          alt=""
-          class="w-12 h-12 rounded-full"
-        />
-        <div class="flex items-center mx-auto">
-          <h3
-            class="text-xl font-normal leading-normal text-center text-black dark:text-white"
-          >
-            বিফ কাবাব
-          </h3>
-        </div>
-      </router-link>
-    </div>
-    <!-- recipe item stop here -->
+    <RecipeItem />
   </div>
 </template>
 <style scoped></style>
