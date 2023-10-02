83e7d1c4a2dfcbe56d77888a59a96a907a868fad
monzuralam <monzur707@gmail.com>
2023-10-02 16:59:31 +0600

    Recipes iterate & show

--- src/components/Recipe/RecipeItem.vue
+++ src/components/Recipe/RecipeItem.vue
@@ -1,18 +1,21 @@
-<script setup></script>
+<script setup>
+const props = defineProps({
+  recipe: Object,
+});
+const id = props?.recipe?.id;
+const title = props?.recipe?.title;
+const img = props?.recipe?.img;
+</script>
 
 <template>
   <div class="border rounded-md shadow p-1.5 mb-2.5">
-    <router-link to="/recipes/recipe/1" class="flex">
-      <img
-        src="../../assets/images/category/kabab.jpg"
-        alt=""
-        class="w-12 h-12 rounded-full"
-      />
+    <router-link :to="'/recipes/recipe/' + id" class="flex">
+      <img :src="img" :alt="title" class="w-12 h-12 rounded-full" />
       <div class="flex items-center mx-auto">
         <h3
           class="text-xl font-normal leading-normal text-center text-black dark:text-white"
         >
-          বিফ কাবাব
+          {{ title }}
         </h3>
       </div>
     </router-link>
--- src/routes/Recipes.vue
+++ src/routes/Recipes.vue
@@ -1,9 +1,21 @@
 <script setup>
 import RecipeItem from "../components/Recipe/RecipeItem.vue";
+import { useRecipes } from "../stores/recipes";
+// retrive id
+const url = window.location.pathname;
+const id = url.substring(url.lastIndexOf("/") + 1);
+// get data from store
+const recipeStore = useRecipes();
+const recipe = recipeStore.recipe;
+const currentRecipes = recipe.filter((recipe) => recipe.category == id);
 </script>
 <template>
   <div class="container h-screen py-16 dark:bg-black">
-    <RecipeItem />
+    <RecipeItem
+      v-for="currentRecipe in currentRecipes"
+      :key="currentRecipe.id"
+      :recipe="currentRecipe"
+    />
   </div>
 </template>
 <style scoped></style>
