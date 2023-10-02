8d3bf4a66e155be380562c3c0c963073fe4443fb
monzuralam <monzur707@gmail.com>
2023-10-02 17:24:22 +0600

    RecipeItem Error component has been added

---
+++ src/components/Recipe/RecipeItemError.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <div class="border rounded-md shadow p-1.5 mb-2.5">
+    <h3
+      class="text-xl font-normal leading-normal text-center text-black dark:text-white"
+    >
+      দুঃখিত! এই বিভাগে কোনো রেসিপি নেই।
+    </h3>
+  </div>
+</template>
+
+<style scoped></style>
