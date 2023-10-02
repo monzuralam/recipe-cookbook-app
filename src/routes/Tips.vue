24c264d2296f17c922c797826255df7d866a714a
monzuralam <monzur707@gmail.com>
2023-10-02 17:48:45 +0600

    TipsItem Iterate

--- src/routes/Tips.vue
+++ src/routes/Tips.vue
@@ -1,10 +1,13 @@
 <script setup>
 import TipsItem from "../components/Tips/TipsItem.vue";
+import { useRecipes } from "../stores/recipes";
+const recipeStore = useRecipes();
+const tipsItem = recipeStore?.tips;
 </script>
 
 <template>
-  <div class="container h-screen py-16 dark:bg-black">
-    <TipsItem />
+  <div class="container overflow-y-scroll h-screen py-16 dark:bg-black">
+    <TipsItem v-for="tips in tipsItem" :key="tips?.id" :data="tips" />
   </div>
 </template>
 
