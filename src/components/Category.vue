dc6fe935c7cb82f295b87e03dacae5ee352c2588
monzuralam <monzur707@gmail.com>
2023-10-02 15:34:59 +0600

    create category component & added

---
+++ src/components/Category.vue
@@ -0,0 +1,22 @@
+<script setup></script>
+
+<template>
+  <!-- category item start here -->
+  <div class="rounded-md border">
+    <router-link to="/recipes/1">
+      <img
+        src="../assets/images/category/biriyani.jpg"
+        alt="Recipe"
+        class="rounded-t-md max-w-full h-28 object-cover"
+      />
+      <h3
+        class="text-lg font-medium leading-normal text-center dark:text-white py-1.5"
+      >
+        বিরিয়ানি রেসিপি
+      </h3>
+    </router-link>
+  </div>
+  <!-- category item stop here -->
+</template>
+
+<style scoped></style>
--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -1,26 +1,12 @@
 <script setup>
 import "@fontsource/hind-siliguri/500.css";
+import Category from "../components/Category.vue";
 </script>
 
 <template>
   <div class="container h-screen overflow-y-scroll py-16 dark:bg-black">
     <div class="grid grid-cols-2 gap-2">
-      <!-- category item start here -->
-      <div class="rounded-md border">
-        <router-link to="/recipes/1">
-          <img
-            src="../assets/images/category/biriyani.jpg"
-            alt="Recipe"
-            class="rounded-t-md max-w-full h-28 object-cover"
-          />
-          <h3
-            class="text-lg font-medium leading-normal text-center dark:text-white py-1.5"
-          >
-            বিরিয়ানি রেসিপি
-          </h3>
-        </router-link>
-      </div>
-      <!-- category item stop here -->
+      <Category />
     </div>
   </div>
 </template>
